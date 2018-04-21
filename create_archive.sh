0f8fcd42252cbe69b15ac0d5d0cb08c55a8b6ff7
John VanderPol <john.vanderpol@gmail.com>
2018-04-21 17:13:54 -0500

    Updated create_archive.sh to fail if there are uncommitted changes

--- create_archive.sh
+++ create_archive.sh
@@ -1,2 +1,8 @@
-git archive -o hcs.zip HEAD
-/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "https://chrome.google.com/webstore/developer/edit/cnmengpimakoahjflcbnpdbaoaaehema"
+rm hcs.zip
+if git diff-index --quiet HEAD --; then
+  echo "Creating archive"
+  git archive -o hcs.zip HEAD
+  /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "https://chrome.google.com/webstore/developer/edit/cnmengpimakoahjflcbnpdbaoaaehema"
+else
+  echo "Unmodified changes, commit before creating archive"
+fi
