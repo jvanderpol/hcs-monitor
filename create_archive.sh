8a806f361966d78a8491bf6893b2fe86cbd31242
John VanderPol <john.vanderpol@gmail.com>
2018-04-22 18:58:23 -0500

    Updated create_archive to fail on uncomitted changes and to automatically tag a release

--- create_archive.sh
+++ create_archive.sh
@@ -1,7 +1,15 @@
-rm hcs.zip
 if git diff-index --quiet HEAD --; then
-  echo "Creating archive"
-  git archive -o hcs.zip HEAD
+  version=`cat manifest.json | python -c "import sys, json; print(json.load(sys.stdin))['version']"`
+  archive_file=hcs-$version.zip
+  echo "Tagging $version"
+  git tag $version
+  if [ $? -ne 0 ]; then
+    echo "Tagging failed, use 'git tag -d $version' if this tag can be deleted"
+    exit 1
+  fi
+  echo "Creating archive $archive_file"
+  rm -f $archive_file
+  git archive -o $archive_file $version
   /Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome "https://chrome.google.com/webstore/developer/edit/cnmengpimakoahjflcbnpdbaoaaehema"
 else
   echo "Unmodified changes, commit before creating archive"
